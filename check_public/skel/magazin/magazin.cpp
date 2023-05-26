9f20baf4b91c0ee51a471f7aabc71c13eb24e030
Diana Maria Stefan <dianastefan.867@gmail.com>
2023-05-26 19:39:49 +0300

    30p - magazin

--- check_public/skel/magazin/magazin.cpp
+++ check_public/skel/magazin/magazin.cpp
@@ -7,45 +7,41 @@ static constexpr int NMAX = (int)1e5 + 5; // 10^5 + 5 = 100.005
 class Task
 {
 public:
-	int N; // N = numar de noduri
-	int Q; // Q = numar de intrebari
-	vector<int> adj[NMAX];
+	int N;				   // N = number of nodes
+	int Q;				   // Q = number of questions
+	vector<int> adj[NMAX]; // adj = adjacency list
 
 	void solve()
 	{
 		ifstream fin("magazin.in");
 		fin >> N >> Q;
 
-		// std::vector<int> p(N + 1, -1); // Parent array
-
 		for (int i = 2; i <= N; ++i)
 		{
-			int x;
+			int x; // x = parent of i
 			fin >> x;
-			adj[x].push_back(i);
+			adj[x].push_back(i); // complete the adjacency list
 		}
 
-		std::vector<int> dfs(N + 1, -1); // dfs array, initially set to -1
-		std::vector<int> dfs_invers(N + 1, -1);
-		std::vector<int> start(N + 1);	// Start time array
-		std::vector<int> finish(N + 1); // Finish time array
+		vector<int> dfs(N + 1, -1);			// dfs array, initially set to -1
+		vector<int> dfs_indexes(N + 1, -1); // array of indexes in dfs array, initially set to -1
+		vector<int> start(N + 1);			// start time array
+		vector<int> finish(N + 1);			// finish time array
+		vector<int> visited(N + 1, -1);		// visited array, initially set to -1
 
-		DFS(N, dfs, dfs_invers, start, finish);
+		int timestamp = 0;
+		dfs[1] = 1;
+		dfs_indexes[1] = 1;
+
+		DFS_RECURSIVE(1, visited, dfs, dfs_indexes, start, finish, timestamp);
+
 		ofstream fout("magazin.out");
-		// for (int i = 1; i < N; ++i)
-		// {
-		// 	fout << dfs[i] << " ";
-		// }
-		// fout << '\n';
-		// fout.close();
 
 		for (int i = 0; i < Q; ++i)
 		{
 			int D; // D = nodul de start
 			int E; // E = numarul de expedieri consecutive
 			fin >> D >> E;
-
-			int pos_D = -1;
 
 			int D_children = (finish[D] - start[D] - 1) / 2;
 			if (D_children < E)
@@ -54,24 +50,7 @@ public:
 				continue;
 			}
 
-			pos_D = dfs_invers[D];
-
-			// for (int i = 1; i <= N; ++i)
-			// {
-			// 	if (dfs[i] == D)
-			// 	{
-			// 		pos_D = i;
-			// 		break;
-			// 	}
-			// }
-
-			// if (pos_D == -1)
-			// {
-			// 	fout << "error" << '\n';
-			// 	continue;
-			// }
-
-			int pos = pos_D + E;
+			int pos = dfs_indexes[D] + E;
 
 			fout << dfs[pos] << '\n';
 		}
@@ -83,39 +62,21 @@ public:
 private:
 	int index = 2;
 
-	void DFS_RECURSIVE(int node, int N, std::vector<int> &visited,
-					   std::vector<int> &dfs, std::vector<int> &dfs_invers, std::vector<int> &start, std::vector<int> &finish, int &timestamp)
+	void DFS_RECURSIVE(int node, std::vector<int> &visited,
+					   std::vector<int> &dfs, std::vector<int> &dfs_indexes, std::vector<int> &start, std::vector<int> &finish, int &timestamp)
 	{
 		start[node] = ++timestamp;
 		for (int neigh : adj[node])
 		{
 			if (visited[neigh] == -1)
 			{
-				dfs_invers[neigh] = index;
+				dfs_indexes[neigh] = index;
 				dfs[index++] = neigh;
 				visited[neigh] = 1;
-				DFS_RECURSIVE(neigh, N, visited, dfs, dfs_invers, start, finish, timestamp);
+				DFS_RECURSIVE(neigh, visited, dfs, dfs_indexes, start, finish, timestamp);
 			}
 		}
 		finish[node] = ++timestamp;
-	}
-
-	void DFS(int N, std::vector<int> &dfs, std::vector<int> &dfs_invers,
-			 std::vector<int> &start, std::vector<int> &finish)
-	{
-		std::vector<int> visited(N + 1, -1); // visited array, initially set to -1
-
-		int timestamp = 0;
-		dfs[1] = 1;
-		dfs_invers[1] = 1;
-
-		for (int node = 1; node <= N; ++node)
-		{
-			if (visited[node] == -1)
-			{
-				DFS_RECURSIVE(node, N, visited, dfs, dfs_invers, start, finish, timestamp);
-			}
-		}
 	}
 };
 
