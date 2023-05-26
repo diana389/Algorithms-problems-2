55b20ccb6023197be34aecf1157defb152aac645
Diana Maria Stefan <dianastefan.867@gmail.com>
2023-05-26 20:10:41 +0300

    updates magazin

--- check_public/skel/magazin/magazin.cpp
+++ check_public/skel/magazin/magazin.cpp
@@ -1,8 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// numarul maxim de noduri
 static constexpr int NMAX = (int)1e5 + 5; // 10^5 + 5 = 100.005
+vector<int> dfs(((int)1e5 + 5), -1);
+vector<int> dfs_indexes(((int)1e5 + 5), -1); // array of indexes in dfs array, initially set to -1
+vector<int> start(((int)1e5 + 5));			 // start time array
+vector<int> finish(((int)1e5 + 5));			 // finish time array
+vector<int> visited(((int)1e5 + 5), -1);	 // visited array, initially set to -1
 
 class Task
 {
@@ -23,17 +27,10 @@ public:
 			adj[x].push_back(i); // complete the adjacency list
 		}
 
-		vector<int> dfs(N + 1, -1);			// dfs array, initially set to -1
-		vector<int> dfs_indexes(N + 1, -1); // array of indexes in dfs array, initially set to -1
-		vector<int> start(N + 1);			// start time array
-		vector<int> finish(N + 1);			// finish time array
-		vector<int> visited(N + 1, -1);		// visited array, initially set to -1
-
-		int timestamp = 0;
 		dfs[1] = 1;
 		dfs_indexes[1] = 1;
 
-		DFS_RECURSIVE(1, visited, dfs, dfs_indexes, start, finish, timestamp);
+		DFS_RECURSIVE(1);
 
 		ofstream fout("magazin.out");
 
@@ -43,7 +40,7 @@ public:
 			int E; // E = numarul de expedieri consecutive
 			fin >> D >> E;
 
-			int D_children = (finish[D] - start[D] - 1) / 2;
+			int D_children = (finish[D] - start[D] - 1) / 2; // number of children of D
 			if (D_children < E)
 			{
 				fout << -1 << '\n';
@@ -52,7 +49,7 @@ public:
 
 			int pos = dfs_indexes[D] + E;
 
-			fout << dfs[pos] << '\n';
+			fout << dfs[pos] << '\n'; // print the answer
 		}
 
 		fout.close();
@@ -61,28 +58,28 @@ public:
 
 private:
 	int index = 2;
+	int timestamp = 0;
 
-	void DFS_RECURSIVE(int node, std::vector<int> &visited,
-					   std::vector<int> &dfs, std::vector<int> &dfs_indexes, std::vector<int> &start, std::vector<int> &finish, int &timestamp)
+	void DFS_RECURSIVE(int node)
 	{
-		start[node] = ++timestamp;
-		for (int neigh : adj[node])
+		start[node] = ++timestamp;	// set the start time of the node
+		for (int neigh : adj[node]) // for each child of the node
 		{
-			if (visited[neigh] == -1)
+			if (visited[neigh] == -1) // if the child is not visited
 			{
 				dfs_indexes[neigh] = index;
 				dfs[index++] = neigh;
 				visited[neigh] = 1;
-				DFS_RECURSIVE(neigh, visited, dfs, dfs_indexes, start, finish, timestamp);
+				DFS_RECURSIVE(neigh);
 			}
 		}
-		finish[node] = ++timestamp;
+		finish[node] = ++timestamp; // set the finish time of the node
 	}
 };
 
 int main()
 {
-	auto *task = new (nothrow) Task(); // hint: cppreference/nothrow
+	auto *task = new (nothrow) Task();
 	if (!task)
 	{
 		cerr << "new failed: WTF are you doing? Throw your PC!\n";
